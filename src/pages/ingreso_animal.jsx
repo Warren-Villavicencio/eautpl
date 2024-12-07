578618a396ee6128f3cbf011ab69dbfa82dcdcb7
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-06 21:16:50 -0500

    commit

--- src/pages/ingreso_animal.jsx
+++ src/pages/ingreso_animal.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ingreso_animal = () => {
   return (
     <div>
-      <h1>Ingreso de Animal</h1>
+      <h1>Ingreso de Animal </h1>
       {/* Resto del contenido del componente */}
     </div>
   );
