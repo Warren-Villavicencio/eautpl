1c3d08b27ad7e0bc74e4d40f47b59ce41d511737
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-06 22:48:31 -0500

    commit

---
+++ src/pages/leche.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const leche = () => {
+  return (
+    <div>
+      <h1>leche </h1>
+      {/* Resto del contenido del componente */}
+    </div>
+  );
+};
+
+export default leche;
