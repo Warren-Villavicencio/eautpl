1d4881b292e518b739e5fed57c6627f554c2863b
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-06 21:08:08 -0500

    commit

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,7 +4,9 @@ const inicio = () => {
   return (
     <>
       <div><h1>Estacion Agropecuaria UTPL</h1></div>
-      <Link href="produccion_leche/Ingreso_animal">Ir a mi Ingreso_animal</Link>
+      <Link href=".produccion_leche/ingreso_animal">Ir a mi Ingreso_animal</Link>
+
+      <Link href="ingreso_animal">Ir a mi 3</Link>
     </>
   );
 };
---
+++ src/pages/ingreso_animal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ingreso_animal = () => {
+  return (
+    <div>
+      <h1>Ingreso de Animal</h1>
+      {/* Resto del contenido del componente */}
+    </div>
+  );
+};
+
+export default ingreso_animal;
