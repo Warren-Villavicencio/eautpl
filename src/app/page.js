a03c6d977b895918e2b0fec2781fb1975aa782b2
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-06 21:39:30 -0500

    Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <Link href="/inicio">inicio</Link>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
