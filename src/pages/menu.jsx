0d6881d12ed305097c10e4030735c74b88fd118a
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-07 06:17:31 -0500

    commit

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,11 +1,23 @@
 import Link from 'next/link';
+import Image from "next/image";
 
 const menu = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link href="/reproduccion">Reproducción</Link>
+          <Link href="/reproduccion">
+          <Image
+          className="dark:invert"
+          src="/reproduccion.png"
+          alt="reproduccion"
+          width={100}
+          height={100}
+          priority
+        />
+          </Link>
+
+
         </li>
         <li>
           <Link href="/produccionDeLeche">Producción de Leche</Link>
