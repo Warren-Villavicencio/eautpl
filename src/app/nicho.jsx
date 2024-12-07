0bd9f660cc813a94ee82c5073e5dcac5efb82509
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-06 21:41:54 -0500

    commit

---
+++ src/app/nicho.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const inicio = () => {
+  return (
+    <>
+      <div><h1>Estacion Agropecuaria UTPL</h1></div>
+      
+
+      <Link href="/ingreso_animal">Ir a mi 3</Link>
+    </>
+  );
+};
+
+export default inicio;
