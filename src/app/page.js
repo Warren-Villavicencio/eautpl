fd6705339e05858ef1e0e7644754eec451c96c06
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-06 21:52:36 -0500

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
           </li>
           
           <li>Save and see your changes instantly.</li>
-          <Link href="/inicio">inicio</Link>
+          <Link href="/inicios">inicio</Link>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
