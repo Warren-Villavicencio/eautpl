db1393d23a96cb047aed4c9c129b83686a2dddd3
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-07 23:00:31 -0500

    commit

--- src/pages/reproduccion.jsx
+++ src/pages/reproduccion.jsx
@@ -3,6 +3,25 @@ const reproduccion = () => {
     return (
       <div>
         <h1>reproduccion</h1>
+        <h2>Formulario de Contacto</h2>
+    <div>
+        <label for="nombre">Nombre:</label>
+        
+    </div>
+    <div>
+        <label for="apellido">Apellido:</label>
+        
+    </div>
+    <div>
+        <label for="email">Correo Electrónico:</label>
+        
+    </div>
+    <div>
+        <label for="mensaje">Mensaje:</label>
+        <textarea id="mensaje" name="mensaje" required></textarea>
+    </div>
+
+
       </div>
     );
   };
