965847bb9cc256077021f4d620814a746753236a
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-08 02:43:44 -0500

    commit

--- src/pages/reproduccion.jsx
+++ src/pages/reproduccion.jsx
@@ -1,29 +1,19 @@
 import './estilos.css';
-const reproduccion = () => {
-    return (
-      <div>
-        <h1>reproduccion</h1>
-        <h2>Formulario de Contacto</h2>
-    <div>
-        <label for="nombre">Nombre:</label>
-        
-    </div>
-    <div>
-        <label for="apellido">Apellido:</label>
-        
-    </div>
-    <div>
-        <label for="email">Correo Electrónico:</label>
-        
-    </div>
-    <div>
-        <label for="mensaje">Mensaje:</label>
-        <textarea id="mensaje" name="mensaje" required></textarea>
-    </div>
 
-
+const Apareamiento = () => {
+  return (
+    <div className="apareamiento-container">
+      <h2>Registro de Apareamiento</h2>
+      <div className="info-container">
+        <p>ID de Apareamiento: APO01</p>
+        <p>Fecha de Apareamiento: 2024-06-05</p>
+        <p>ID del Macho: M001</p>
+        <p>ID de la Hembra: H002</p>
+        <p>Propósito del Apareamiento: Producción de Leche</p>
+        <p>Método de Apareamiento: Inseminación Artificial</p>
       </div>
-    );
-  };
-  
-  export default reproduccion;
+    </div>
+  );
+};
+
+export default Apareamiento;
