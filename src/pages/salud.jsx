1fa2e6d9ef5414c16649330869663b1ff4d92c8e
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-07 21:05:01 -0500

    commit

--- src/pages/salud.jsx
+++ src/pages/salud.jsx
@@ -1,10 +1,24 @@
 import './estilos.css';
-const salud = () => {
-    return (
-      <div>
-        <h1>salud</h1>
+
+const Salud = () => {
+  return (
+    <div className="salud-container">
+      <h2>Historia Clínica</h2>
+      <div className="info-container">
+        <p>Número de Identificación: 00123</p>
+        <p>Fecha de Revisión: 20/05/2024</p>
+        <p>Veterinario: Dr. Martínez</p>
       </div>
-    );
-  };
-  
-  export default salud;
+      <h3>Vacunaciones</h3>
+      <ul>
+        <li>Brucelosis: 15/05/2024</li>
+      </ul>
+      <h3>Desparasitaciones</h3>
+      <ul>
+        <li>Ivermectina: 20/05/2024</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Salud;
