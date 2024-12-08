4f5663ca40f9df9210771c79548d56e47b409d72
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-12-08 02:46:47 -0500

    commit

--- src/pages/produccionDeLeche.jsx
+++ src/pages/produccionDeLeche.jsx
@@ -1,10 +1,18 @@
 import './estilos.css';
-const produccionDeLeche = () => {
-    return (
-      <div>
-        <h1>produccionDeLeche</h1>
+
+const ProduccionLeche = () => {
+  return (
+    <div className="produccion-leche-container">
+      <h2>Producción de Leche</h2>
+      <div className="info-container">
+        <p>Número de Identificación: 00123</p>
+        <p>Raza: Holstein</p>
+        <p>Producción de Leche: 25L/día</p>
+        <p>Estado de Salud: Saludable</p>
+        <p>Localización: Sector A</p>
       </div>
-    );
-  };
-  
-  export default produccionDeLeche;
+    </div>
+  );
+};
+
+export default ProduccionLeche;
